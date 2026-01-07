755f0eeb9235fc774cbec48bb57fa6e02b86ad77
youarerude <ohmydays120@gmail.com>
2026-01-08 07:33:33 +0800

    Adjust suit color values for damage types

--- IllusionAlt.lua
+++ IllusionAlt.lua
@@ -23,11 +23,11 @@ local ordealLoops = {}
 local currentSuit = "Standard Suit"
 local suits = {
     ["Standard Suit"] = {
-        Crimson = 1,
-        Blue = 1,
-        Purple = 1.5,
-        White = 2,
-        Grey = 2
+        Crimson = 0.3,
+        Blue = 0.3,
+        Purple = 0.3,
+        White = 0.5,
+        Grey = 0.5
     }
 }
 
